5bdfb0508412f7cf416f727b33cb7484a90323df
prajakta.hadole <prajakta.hadole@humancloud.co.in>
2023-12-12 13:38:07 +0530

    poc-1

---
+++ Structure, Data, Interpolation, methods/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <Hello></Hello>
+  <Home></Home>
+
+
+</template>
+
+
+
+<script >
+import Home from './components/Home.vue'
+import Hello from './components/Hello.vue'
+  
+
+export default {
+    name : "App",
+    components: {
+        Home,
+        Hello,
+   }
+
+}
+
+
+
+
+
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ Structure, Data, Interpolation, methods/src/components/Home.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="border1px">
+
+        <h1>Home</h1>
+        <h5>Name : {{name}}</h5>
+        <h6>Full Name : {{getName("Prajkta Haadole") }}</h6>
+        <h6>Email : {{ email }}</h6>
+        <h6>Property : {{ 10+15+ 50 }}Cr</h6>
+        <h6>Mobile : {{mobile}}</h6>
+        <h6>Mobile : {{mobile=11111111}}</h6>          
+         <!-- this value we can assigned and change in template by interpolation -->
+        <h6>Mobile : {{mobile==11111111}}</h6>
+        <h6>Age : {{getAge()}} Years</h6> 
+        <h6> family details : 
+            [ Father Name :{{ getparentDetails().fatherName}},
+            <br>
+              Mother Name :{{ getparentDetails().motherName}},
+              <br>
+              Sister Name :{{ getSiblingsDetails().sisterName}},
+              <br>
+              Brother Name :{{ getSiblingsDetails().brotherName}},
+              <br>
+                conatactId : {{ getparentDetails().email }}
+                <!-- RETURN SAME contact ID   -->
+            ]</h6> 
+
+       
+    </div>
+   
+</template>
+<script>
+
+export default {
+    name : "Home",
+    data(){
+        return {
+            technologies : ["javascript", "java", "python"],
+            name : "Prajakta",
+            email: "prajaktahadole@gmail.com",
+            mobile: 874185296,
+            getName : function(x){
+                return x;
+            },
+           
+        }
+    },
+
+    methods : {
+        getAge(){
+          return 22;
+      },
+
+      getparentDetails(){
+          return {
+                fatherName : "Vijay Hadole",
+                motherName : "Jaya Hadole",
+                email : this.email,
+            };
+      },
+      getSiblingsDetails(){
+        return {
+            sisterName: "Sakshi Hadole",
+            brotherName : "Parth Hadole",
+        }
+      },
+
+      getOtherDetails (){
+        return {
+            nation : 'India',
+            maritailStatus : "Single",
+
+        }
+      }
+      
+    },
+
+}
+
+</script>
+
+<style scoped>
+.border1px{
+    border: 5px solid red;
+    padding: 15px;
+    margin: auto;
+}
+
+</style>
